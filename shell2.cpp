0de89e31ea75d04b76f66594fc12cc680dfad729
kritika-075 <kritika.singh@students.iiit.ac.in>
2019-09-09 02:17:01 +0530

    second commit

--- shell2.cpp
+++ shell2.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include<fcntl.h>
 #define READ_BUFFER 1024
 #define MAXSIZE 1024
 
@@ -12,6 +13,27 @@ char *arr[] = {"help","cd","exit"};
 int change_dir(char **args);
 int help_1(char **args);
 int exit_1(char **args);
+int pipe_count=0;
+
+void initialize()
+{
+	FILE *fd;
+	fd=fopen("bash1.txt","w+");
+	char *str1;char *str2;char *str3;char *str4;
+	str1=getenv("PATH");
+	str2=getenv("USER");
+	str3=getenv("HOME");
+	str4=getenv("USERNAME");
+	fputs(str1,fd);
+	fputs("\n",fd);
+	fputs(str2,fd);
+	fputs("\n",fd);
+	fputs(str3,fd);
+	fputs("\n",fd);
+	fputs(str4,fd);
+	fputs("\n",fd);
+	fclose(fd);
+}
 
 int change_dir(char **args)
 {
@@ -52,14 +74,14 @@ int execute(char **args)
 
 else
 {
-	 pid_t pid, wpid;
+	   pid_t pid, wpid;
 		int stat;
 		pid = fork();
 		if (pid < 0)
-	{
+		{
 			printf("error");
 			exit(EXIT_FAILURE);
-	}
+		}
 		else if (pid == 0) 
 		{
 				if (execvp(args[0], args) == -1)
@@ -83,7 +105,6 @@ static int first=1;
 	}
 	printf("->");
 }
-
 char** separate_line(char *l)
 {
 	int buff=MAXSIZE;
@@ -114,10 +135,81 @@ char** separate_line(char *l)
 	split[++pos]=NULL;
 	return split;
 }
+int exe_pipe(char **arr)
+{   
+	int p=0;
+	int fd[2];
+	int pid;
+	int c=0;
+	while(*arr !=NULL)
+	{
+		pipe(fd);
+		pid = fork();//creating a child process
+		if(pid==-1)
+		{
+			printf("Error forking!\n");
+			return 1;
+		}
+		else if(pid ==0)//child has been created
+  		{
+    	dup2(p,0);
+    	if(*(arr+1) !=NULL)
+    	dup2(fd[1],1);
+    	close(fd[0]);
+    	close(fd[1]);
+    	char **inp;
+    inp=separate_line(arr[c]);
+    /// to check for exception commands like redirection chdir etc
+    if(execvp(inp[0],inp)==-1)
+    	printf("Command not executed!");
+    return 1;
+   }
+   else
+   {
+   	wait(NULL);
+   	close(fd[1]);
+   	p=fd[0];
+	arr++;
+   	}	
+	}
+pipe_count=0;
+return 1;
+}
+
+char** piped(char *l)
+{
+int buff=MAXSIZE;
+	int pos=-1;
+	char **split=(char **)malloc(buff * sizeof(char));
+	char *tok;
+	if(!split)
+	{
+		fprintf(stderr,"No Memory allocated");
+		exit(EXIT_FAILURE);
+	}
+	tok=strtok(l,"|");
+	while(tok!=NULL)
+	{
+		split[++pos]=tok;
+		if(pos==(buff-1))
+		{
+			buff=buff+MAXSIZE;
+			split=(char**)realloc(split,buff *sizeof(char));
+			if(!split)
+			{
+			fprintf(stderr,"No memory allocated");
+			exit(EXIT_FAILURE);
+			}
+		}
+		tok=strtok(NULL,"|");
+	}
+	split[++pos]=NULL;
+	return split;
+}
 
 char *get_line()
 {
-	int buff=MAXSIZE;int c;
+	int buff=MAXSIZE;int c;int redirection=0;
 	char * input=(char*)malloc(buff *sizeof(char));
 	int p=-1;
 	if(input==NULL)
@@ -130,6 +222,10 @@ char *get_line()
 		while(true)
 		{
 			c=getchar();
+			if(c=='|')
+			pipe_count=1;
+			if(c=='>')
+			redirection=1;
 			if(p>=(buff-1))
 		  {
 				buff=buff+MAXSIZE;
@@ -148,17 +244,37 @@ char *get_line()
 	}	
 }
 }
+//HISTORY
+void add_history(char *l)
+{
+	FILE *fp;
+	fp=fopen("history.txt","a");
+	fputs(l,fp);
+	fclose(fp);
+}
+//MAIN FUNCTION HERE
 int main(int argc,char **argv)
 {
 		int stat;
-		char **arr;
-		char *l;
+		initialize();
 		do
 		{
+			char **arr;
+		   char **split;
+		   char *l;
 			prompt();
 			l=get_line();
-			arr=separate_line(l);
-			stat=execute(arr);
+			add_history(l);
+			if(pipe_count!=0)
+			{
+				split=piped(l);
+				stat=exe_pipe(split);
+			}
+		else
+		{
+		arr=separate_line(l);
+		stat=execute(arr);
+		}
 		}
 		while(stat);
 		return 1;
